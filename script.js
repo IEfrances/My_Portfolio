d262946a7ccab5ec57966884e6ad09583f82e62d
Frances <francesiigwe@gmail.com>
2023-12-22 08:24:17 -0700

    updating profile

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.querySelector('.butterfly-icon').addEventListener('click', function() {
+    document.getElementById('about-me').scrollIntoView({ behavior: 'smooth' });
+ });
+
+
+document.querySelector('.butterfly-icon2').addEventListener('click', function() {
+   document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+});
+
+
